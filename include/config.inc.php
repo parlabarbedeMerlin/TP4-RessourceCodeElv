f8e15f76e255a51813206ad9da26bb049448de1d
parlabarbedeMerlin <tristan.tr.renard@gmail.com>
2022-01-18 16:14:36 +0100

    Bug fix

--- include/config.inc.php
+++ include/config.inc.php
@@ -2,10 +2,10 @@
 // Param�tres de l'application Covoiturage
 // A modifier en fonction de la configuration
 
-define('DBHOST', "localhost");
+define('DBHOST', "127.0.0.1");
 define('DBNAME', "vente_composants");
 define('DBUSER', "root");
-define('DBPASSWD', "Ar.Tr.re21102004");
+define('DBPASSWD', "");
 define('ENV','dev');
 // pour un environememnt de production remplacer 'dev' (d�veloppement) par 'prod' (production)
 // les messages d'erreur du SGBD s'affichent dans l'environememnt dev mais pas en prod
